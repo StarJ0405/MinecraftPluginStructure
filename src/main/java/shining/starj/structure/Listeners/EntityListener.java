f66517cb78c3e0690c4448f0e2ce529e1dc8bfae
StarJ0405 <ghdtjdwo126@naver.com>
2024-03-07 01:51:22 +0900

    cì¶추가

---
+++ src/main/java/shining/starj/structure/Listeners/EntityListener.java
@@ -0,0 +1,6 @@
+package shining.starj.structure.Listeners;
+
+import org.bukkit.event.Listener;
+
+public class EntityListener implements Listener {
+}
---
+++ src/main/java/shining/starj/structure/Listeners/PlayerListener.java
@@ -0,0 +1,18 @@
+package shining.starj.structure.Listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerListener implements Listener {
+    @EventHandler
+    public void Events(PlayerJoinEvent e) {
+        // 플레이어 접속시 발생하는 이벤트
+    }
+
+    @EventHandler
+    public void Events(PlayerQuitEvent e){
+        // 플레이어가 서버에서 나갈시 발생
+    }
+}
